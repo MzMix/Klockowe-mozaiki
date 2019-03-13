9a053a7b3f6fdaf9ed2121d5058df068a6131d46
Mikołaj Zuziak <miki.zuziak@gmail.com>
2019-03-13 19:29:08 +0100

    Update - board, colors

--- sketch.js
+++ sketch.js
@@ -5,24 +5,27 @@ Number.prototype.between = function (a, b) {
     return this > minVal && this < maxVal;
 };
 
-function for2dSquareLoop(max, fxn) {
-    for (let i = 0; i < max; i++) {
-        for (let j = 0; j < max; j++) {
+function forSquare2dLoop(max, fxn) {
+
+    for (let j = 0; j < max; j++) {
+        for (let i = 0; i < max; i++) {
             fxn(i, j);
         }
     }
 }
 
 function setup() {
-    createCanvas(Global.segmentsPerSide * Global.segmentSize + 1, Global.segmentsPerSide * Global.segmentSize + 1);
+    let c = createCanvas((Global.segmentsPerSide + 2) * Global.segmentSize + 1, (Global.segmentsPerSide + 2) * Global.segmentSize + 2);
+    select('.box').child(c);
 
     Global['Gui'] = new Gui();
-    Global.Gui.createBoard();
+    Global.Gui.createBoard().generateColorPick();
 
 }
 
 function draw() {
-    background(175);
+    clear();
+    background(color(0, 0, 0, 0));
 
     for (let s of Global.segments) {
         s.draw();
